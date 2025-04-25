b317244e5aef0b421d2725755f2444b612a365ca
Enigma_Soul <119654171+rongjianuo@users.noreply.github.com>
2025-04-25 22:37:36 +0800

    test

--- test_main.py
+++ test_main.py
@@ -3,6 +3,7 @@ import os
 
 if __name__ == '__main__':
     print('Current working directory:', os.getcwd())
+    os.makedirs("output", exist_ok=True)
     with open("output/test.txt", "w") as f:
         f.write("Hello World!")
     print("File created at:", os.path.abspath("test.txt"))
